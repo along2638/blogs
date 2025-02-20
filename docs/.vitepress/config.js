b72d688c5bf61c715023628e6ac3ab378109c90f
zfl <1587254338@qq.com>
2025-02-20 16:41:44 +0800

    [提交说明]：初始化

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,5 +1,6 @@
 // .vitepress/config.js
 export default {
+    base: '/blogs/', // 设置基础路径为 /repo/, 这样在 GitHub Pages 上才能正常访问
     // 站点级选项
     title: 'VitePress',
     description: 'Just playing around.',
